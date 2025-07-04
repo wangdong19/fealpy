4695f6105443c6b03cc2329ca8293d1b3a39ccb1
wangdong <804778943@qq.com>
2025-07-04 19:48:19 +0800

    update

--- fealpy/opt/PLBFGSAlg.py
+++ fealpy/opt/PLBFGSAlg.py
@@ -27,7 +27,9 @@ class PLBFGS(Optimizer):
         x0: TensorLike,
         objective,
         Preconditioner: MatrixLike = None,
+        update_Preconditioner = None, 
         MaxIters: int = 500,
+        FunValDiff: float = 1e-6,
         StepLengthTol: float = 1e-6,
         NormGradTol: float = 1e-6,
         NumGrad = 10,
@@ -37,7 +39,9 @@ class PLBFGS(Optimizer):
             x0=x0,
             objective=objective,
             Preconditioner=Preconditioner,
+            update_Preconditioner=update_Preconditioner,
             MaxIters=MaxIters,
+            FunValDiff = FunValDiff, 
             StepLengthTol=StepLengthTol,
             NormGradTol=NormGradTol,
             NumGrad=NumGrad
@@ -52,7 +56,6 @@ class PLBFGS(Optimizer):
             rho = bm.set_at(rho, i, 1/bm.dot(self.S[i], self.Y[i]))
             alpha = bm.set_at(alpha, i, bm.dot(self.S[i], q)*rho[i])
             q = q - alpha[i]*self.Y[i]
-
         if self.P is not None:
             r = self.P@q
         else:
@@ -76,7 +79,7 @@ class PLBFGS(Optimizer):
 
         alpha = options["StepLength"]
         if options["Print"]:
-            print('initial: f = {f}, gnorm = {gnorm}')
+            print(f'initial: f = {f}, gnorm = {gnorm}')
 
         flag = 0 # The convergence flag
         j = 0
@@ -96,6 +99,11 @@ class PLBFGS(Optimizer):
             f = falpha
             g = galpha
             gnorm = bm.linalg.norm(g)
+            
+            if options["update_Preconditioner"] is None:
+                pass
+            else:
+                options["update_Preconditioner"](x)
 
             if options["Print"]:
                 print(f'current step {i}, StepLength = {alpha}, ', end='')
